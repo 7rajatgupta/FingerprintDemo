cfcd9bc22b506d939b6b18dd01e3374d2c89a23c
Rajat Gupta <rajatpunkstaa@gmail.com>
2017-05-09 19:42:16 +0530

    initial build

---
+++ app/src/main/java/com/rgapps/fingerprint/Handler.java
@@ -0,0 +1,12 @@
+package com.rgapps.fingerprint;
+
+import android.hardware.fingerprint.FingerprintManager;
+
+/**
+ * Created by rajat on 9/5/17.
+ */
+
+public class Handler extends FingerprintManager.AuthenticationCallback {
+
+
+}
