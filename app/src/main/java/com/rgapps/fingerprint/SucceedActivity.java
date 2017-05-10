effc7daa7faa6efb2324c135d2f7da37df2fce97
Rajat Gupta <rajatpunkstaa@gmail.com>
2017-05-10 18:02:30 +0530

    initial build

--- app/src/main/java/com/rgapps/fingerprint/SucceedActivity.java
+++ app/src/main/java/com/rgapps/fingerprint/SucceedActivity.java
@@ -1,27 +1,36 @@
 package com.rgapps.fingerprint;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class SucceedActivity extends AppCompatActivity {
+
+    private ImageButton mGitbutton,mAboutButton;
+    private String mUrl = "https://github.com/rajatpunkstaa/FingerprintDemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succeed);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().hide();
+
+        mGitbutton = (ImageButton)findViewById(R.id.git_button);
+        mAboutButton = (ImageButton)findViewById(R.id.about_button);
+
+        mGitbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(mUrl));
+                startActivity(i);
             }
         });
     }
